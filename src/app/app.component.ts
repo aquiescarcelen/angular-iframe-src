513f8e6f6640c60b1b3fd365fa2cee608d087995
aquiescarcelen <aquiescarcelen@gmail.com>
2022-06-09 15:51:22 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Componente externo iframe';
-  url: string = "https://localhost:44391/index.aspx";
+  url: string = 'https://recycob.fin.ec/';
   urlSafe: SafeResourceUrl;
 
-  constructor(public sanitizer: DomSanitizer) { }
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 }
